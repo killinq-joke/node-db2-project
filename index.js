eae25e5635d6e382d1e623b0fa187c04b8a0f3e3
Zak <zakadmaster@gmail.com>
2020-03-17 14:29:18 +0100

    Addition of create and read

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 const express = require('express')
+const db = require('./data/config')
 
 const app = express()
 
 const PORT = process.env.PORT || 6000
 
 app.use(express.json())
+
+function get() {
+    return db("cars")
+}
+
+function getById(id) {
+    return db("cars").where({id})
+}
+
+function insert(car) {
+    return db("cars").insert(car)
+    .then(ids => {
+        return getById(ids[0])
+    })
+}
+
+app.get("/", (req, res) => {
+    get()
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).end()
+    })
+})
+
+app.get("/:id", (req, res) => {
+    const {id} = req.params
+    getById(id)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).end()
+    })
+})
+
+app.post("/", (req, res) => {
+    const car = req.body
+    insert(car)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).end()
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
